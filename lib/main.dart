0fe5b48705af6f83e7630d2c5637dc5e15307157
Augus611 <augus611@gmail.com>
2022-06-03 22:23:36 -0300

    Agregué el número del grupo en el login.

--- lib/main.dart
+++ lib/main.dart
@@ -150,118 +150,132 @@ class _LogInState extends State<LogIn> {
       appBar: AppBar(
         title: const Text('Práctica 4'),
       ),
-      body: Container(
-        child: Align(
-          alignment: const Alignment(0,-0.5),
-          child: SizedBox(
-            width: 333,
-            height: 484,
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                children: [
-                  Padding(
-                  padding: const EdgeInsets.all(10),
+      body: DecoratedBox(
+        child: Stack(
+          children: [
+            Align(
+              alignment: const Alignment(0,-0.5),
+              child: SizedBox(
+                width: 333,
+                height: 484,
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
                   child: Column(
-                    children: <Widget>[
-                      Container(
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.all(10),
-                          child: const Text(
-                            'Práctica 4',
-                            style: TextStyle(
-                                color: Colors.black54,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 30),
-                          )),
-                      Container(
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.all(10),
-                          child: const Text(
-                            'Iniciar Sesión',
-                            style: TextStyle(fontSize: 20),
-                          )),
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        child: ValueListenableBuilder(
-                          valueListenable: _controllerUsuario,
-                          builder: (context, value, _) {
-                            return TextField(
-                              controller: _controllerUsuario,
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: InputDecoration(
-                                border: const OutlineInputBorder(),
-                                labelText: 'Nombre de Usuario',
-                                errorText: _submitted ? _textoErrorUsuario : null,
-                              ),
-                            );
-                          }
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                        child: ValueListenableBuilder(
-                          valueListenable: _controllerContrasena,
-                          builder: (context, value, _) {
-                            return TextField(
-                              controller: _controllerContrasena,
-                              obscureText: _isObscure,
-                              decoration: InputDecoration(
-                                border: const OutlineInputBorder(),
-                                labelText: 'Contraseña',
-                                errorText: _submitted ? _textoErrorContrasena : null,
-                                suffixIcon: IconButton(
-                                  icon: Icon(
-                                    _isObscure ? Icons.visibility : Icons.visibility_off),
-                                  onPressed: () {
-                                    setState(() {
-                                      _isObscure = !_isObscure;
-                                    });
-                                  },
-                                )
-                              ),
-                            );
-                          }
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          //forgot password screen
-                        },
-                        child: const Text('Olvidé la contraseña',),
-                      ),
-                      Container(
-                          height: 50,
-                          width: double.infinity,
-                          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          child: ElevatedButton(
-                            child: const Text('Iniciar Sesión'),
-                            onPressed: _iniciarSesion,
-                          )
-                      ),
-                      Row(
+                    children: [
+                      Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
                         children: <Widget>[
-                          const Text('¿No tenés una cuenta?'),
-                          TextButton(
-                            child: const Text(
-                              'Crear cuenta',
-                              style: TextStyle(fontSize: 20),
+                          Container(
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.all(10),
+                              child: const Text(
+                                'Práctica 4',
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 30),
+                              )),
+                          Container(
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.all(10),
+                              child: const Text(
+                                'Iniciar Sesión',
+                                style: TextStyle(fontSize: 20),
+                              )),
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            child: ValueListenableBuilder(
+                              valueListenable: _controllerUsuario,
+                              builder: (context, value, _) {
+                                return TextField(
+                                  controller: _controllerUsuario,
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: InputDecoration(
+                                    border: const OutlineInputBorder(),
+                                    labelText: 'Nombre de Usuario',
+                                    errorText: _submitted ? _textoErrorUsuario : null,
+                                  ),
+                                );
+                              }
                             ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                            child: ValueListenableBuilder(
+                              valueListenable: _controllerContrasena,
+                              builder: (context, value, _) {
+                                return TextField(
+                                  controller: _controllerContrasena,
+                                  obscureText: _isObscure,
+                                  decoration: InputDecoration(
+                                    border: const OutlineInputBorder(),
+                                    labelText: 'Contraseña',
+                                    errorText: _submitted ? _textoErrorContrasena : null,
+                                    suffixIcon: IconButton(
+                                      icon: Icon(
+                                        _isObscure ? Icons.visibility : Icons.visibility_off),
+                                      onPressed: () {
+                                        setState(() {
+                                          _isObscure = !_isObscure;
+                                        });
+                                      },
+                                    )
+                                  ),
+                                );
+                              }
+                            ),
+                          ),
+                          TextButton(
                             onPressed: () {
-                              //signup screen
+                              //forgot password screen
                             },
-                          )
+                            child: const Text('Olvidé la contraseña',),
+                          ),
+                          Container(
+                              height: 50,
+                              width: double.infinity,
+                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                              child: ElevatedButton(
+                                child: const Text('Iniciar Sesión'),
+                                onPressed: _iniciarSesion,
+                              )
+                          ),
+                          Row(
+                            children: <Widget>[
+                              const Text('¿No tenés una cuenta?'),
+                              TextButton(
+                                child: const Text(
+                                  'Crear cuenta',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                onPressed: () {
+                                  //signup screen
+                                },
+                              )
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.center,
+                          ),
                         ],
-                        mainAxisAlignment: MainAxisAlignment.center,
-                      ),
-                    ],
-                  ))
-                ]
-              ),
+                      ))
+                    ]
+                  ),
 
+                ),
+              ),
             ),
+            const Align(
+              alignment: Alignment(0,0.95),
+              child: Text(
+                'Grupo 4 - UTN FRLP',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.w400
+                )
+              )
+            )
+          ],
           ),
-        ),
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/background.gif'),
